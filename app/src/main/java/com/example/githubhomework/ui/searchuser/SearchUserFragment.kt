2f6a8cdcf2dc00e9c8b7f07f75b685bfd096c3df
Aleš Kůtek <kutekales@gmail.com>
2020-04-05 12:08:24 +0200

    show keyboard automatically - search user

--- app/src/main/java/com/example/githubhomework/ui/searchuser/SearchUserFragment.kt
+++ app/src/main/java/com/example/githubhomework/ui/searchuser/SearchUserFragment.kt
@@ -1,9 +1,11 @@
 package com.example.githubhomework.ui.searchuser
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +14,7 @@ import com.example.githubhomework.databinding.FragmentSearchUserBinding
 import com.example.githubhomework.tools.ui.addDivider
 import kotlinx.android.synthetic.main.fragment_search_user.*
 import org.koin.android.ext.android.inject
+
 
 class SearchUserFragment : Fragment() {
     lateinit var viewModel: SearchUserViewModel
@@ -37,12 +40,18 @@ class SearchUserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        requireActivity().actionBar?.setDisplayHomeAsUpEnabled(true)
-//        requireActivity().actionBar?.setIcon(R.drawable.baseline_add_white_18)
-        searchUserInput.requestFocus()
         searchRecycleView.addDivider(requireContext())
 
         viewModel.searchText.observe(viewLifecycleOwner, searchObserver.create(this))
         viewModel.searchResult.observe(viewLifecycleOwner, recycleViewObserver.create(this))
+
+        showKeyboard(searchUserInput)
+    }
+
+    private fun showKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+            imm?.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+        }
     }
 }
