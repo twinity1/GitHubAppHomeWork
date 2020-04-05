4c76e0c67fa8586d56cb584b704431c1ac3fe0a5
Aleš Kůtek <kutekales@gmail.com>
2020-04-05 11:18:04 +0200

    search user empty screen

--- app/src/main/java/com/example/githubhomework/ui/searchuser/RecycleViewObserver.kt
+++ app/src/main/java/com/example/githubhomework/ui/searchuser/RecycleViewObserver.kt
@@ -1,6 +1,7 @@
 package com.example.githubhomework.ui.searchuser
 
 import android.content.Intent
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubhomework.UserActivity
@@ -11,19 +12,46 @@ import com.example.githubhomework.persistence.entities.User
 class RecycleViewObserver {
     fun create(owner: SearchUserFragment): Observer<List<User>> {
         return Observer {
-            owner.binding.searchRecycleView.apply {
-                val userAdapter = UserListAdapter(it)
-                userAdapter.onShowUser = {
-                    val intent = Intent(owner.activity, UserActivity::class.java)
+            setVisibility(it, owner)
+            setRecycleView(owner, it)
+        }
+    }
 
-                    intent.putExtra(UserActivity.EXTRA_GITHUB_REPOSITORY_URL, it.gitHubUser.reposUrl)
+    private fun setRecycleView(
+        owner: SearchUserFragment,
+        it: List<User>
+    ) {
+        owner.binding.searchRecycleView.apply {
+            val userAdapter = UserListAdapter(it)
+            userAdapter.onShowUser = {
+                val intent = Intent(owner.activity, UserActivity::class.java)
 
-                    owner.startActivity(intent)
-                }
+                intent.putExtra(UserActivity.EXTRA_GITHUB_REPOSITORY_URL, it.gitHubUser.reposUrl)
 
-                layoutManager = LinearLayoutManager(owner.activity)
-                adapter = userAdapter
+                owner.startActivity(intent)
             }
+
+            layoutManager = LinearLayoutManager(owner.activity)
+            adapter = userAdapter
+        }
+    }
+
+    private fun setVisibility(
+        it: List<User>,
+        owner: SearchUserFragment
+    ) {
+        if (owner.viewModel.searchText.value == "") {
+            owner.viewModel.itemsVisibility.value = View.GONE
+            owner.viewModel.noFoundTextVisibility.value = View.GONE
+            return
+        }
+
+        if (it.count() == 0) {
+            owner.viewModel.itemsVisibility.value = View.GONE
+            owner.viewModel.noFoundTextVisibility.value = View.VISIBLE
+        } else {
+            owner.viewModel.itemsVisibility.value = View.VISIBLE
+            owner.viewModel.noFoundTextVisibility.value = View.GONE
         }
     }
 }
--- app/src/main/java/com/example/githubhomework/ui/searchuser/SearchUserViewModel.kt
+++ app/src/main/java/com/example/githubhomework/ui/searchuser/SearchUserViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.githubhomework.ui.searchuser
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.githubhomework.persistence.entities.User
@@ -8,4 +9,7 @@ class SearchUserViewModel : ViewModel() {
     val searchText: MutableLiveData<String> = MutableLiveData()
 
     val searchResult: MutableLiveData<List<User>> = MutableLiveData()
+
+    val itemsVisibility = MutableLiveData<Int>(View.GONE)
+    val noFoundTextVisibility = MutableLiveData<Int>(View.GONE)
 }
